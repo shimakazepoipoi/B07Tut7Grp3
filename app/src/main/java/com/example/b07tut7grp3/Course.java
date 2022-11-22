9e1f5422b8f8feec2b453123ead1e62919846401
Kevin Li <kevinc.li@mail.utoronto.ca>
2022-11-22 12:49:04 -0500

    Added course and user classes

---
+++ app/src/main/java/com/example/b07tut7grp3/Course.java
@@ -0,0 +1,5 @@
+package com.example.b07tut7grp3;
+interface Course {
+    public String getCourseId();
+
+}
---
+++ app/src/main/java/com/example/b07tut7grp3/User.java
@@ -0,0 +1,8 @@
+package com.example.b07tut7grp3;
+import java.util.HashSet;
+abstract class User {
+    HashSet<Course> courseList;
+    public User(){
+
+    }
+}
