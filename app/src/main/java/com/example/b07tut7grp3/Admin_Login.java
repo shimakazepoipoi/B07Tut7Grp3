933111d313c2ab7800034d47219b6aca22317aa0
Matthew Iannantuono <miannantuono12@gmail.com>
2022-11-25 12:36:26 -0500

    student course list changes

--- app/src/main/java/com/example/b07tut7grp3/Admin_Login.java
+++ app/src/main/java/com/example/b07tut7grp3/Admin_Login.java
@@ -2,7 +2,6 @@ package com.example.b07tut7grp3;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
